02a11d7df3995d05ea8a7be14866fdb9dbe03a75
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2020-12-21 15:52:03 +0900

    商品情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @items = Item.order("created_at DESC")
@@ -7,5 +7,21 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new  
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :city_id, :days_id, :delivery_id, :status_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -18,8 +18,12 @@ class Item < ApplicationRecord
 
   validates :price, numericality: {
     greater_than_or_equal_to: 300,
-    less_than_or_equal_to: 9,999,999
+    less_than_or_equal_to: 9_999_999
   }  
+
+  validates :price, numericality: {
+    with: /\A[0-9]+\z/
+  }
 
   validates :category_id, :delivery_id, :status_id, :city_id, :days_id, numericality: { other_than: 1 } 
 
