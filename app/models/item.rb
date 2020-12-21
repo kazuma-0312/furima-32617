8d00a7724fae060e126069d16ff3c1f943a7c72c
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2020-12-21 12:21:41 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,17 @@
 class Item < ApplicationRecor
 
   has_one_attached :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :price    
+  end
 
 
-
-
-
-
-
+  validates :category_id, :delivery_id, :status_id, :city_id, :days_id, numericality: { other_than: 1 } 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
