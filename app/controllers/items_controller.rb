8a0f63211462c2469391165047cf0bc6eb4549ec
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2020-12-22 21:29:39 +0900

    商品編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create, :edit]
+before_action :authenticate_user!, except: [:index, :show]
+before_action :group_item, only: [:show, :edit, :update]
 
   def index
   @items = Item.order("created_at DESC")
@@ -19,17 +20,14 @@ before_action :authenticate_user!, only: [:new, :create, :edit]
   end  
 
   def show
-      @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
     
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -42,5 +40,9 @@ before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def item_params
     params.require(:item).permit(:image, :name, :text, :category_id, :city_id, :days_id, :delivery_id, :status_id, :price).merge(user_id: current_user.id)
+  end
+
+  def group_item
+    @item = Item.find(params[:id])
   end
 end
