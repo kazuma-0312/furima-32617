4ec01584e272deb59bd737e39756045ca2b3dd44
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2020-12-22 20:58:23 +0900

    ログイン,出品者ではない人のトップページ遷移設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create]
+before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
   @items = Item.order("created_at DESC")
@@ -24,6 +24,7 @@ before_action :authenticate_user!, only: [:new, :create]
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
   end
     
 
