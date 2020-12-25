ba7f1d4bc40407b698aac068d3600acf8a8c2933
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2020-12-25 19:29:42 +0900

    バリデーションの作成

--- app/models/home_buy.rb
+++ app/models/home_buy.rb
@@ -1,4 +1,20 @@
 class HomeBuy
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :address, :city_id, :municipality, :street, :building,:
+  attr_accessor :user_id, :item_id, :address, :city_id, :municipality, :street, :building,
+
+  with_options presence: true do
+    validates :address, format: { with: /^\d{3}[-]\d{4}$/, message: "code Input correctly"}
+    validates :city_id, numericality: {other_than: 1, message: 'Select'}
+    validates :municipality
+    validates :street
+    validates :number, format: { with: /^\d{11}$/, message: "Input only number" }
+    validates :building
+    validates :user_id
+    validates :item_id
+  end
+
+  def save
+    @buy = Buy.create(user_id: user_id, item_id: item_id)
+    Home.create(address: address, city_id: city_id, municipality: municipality, street: street, number: number, building: building, buy_id: @buy.id)
+  end
 end
