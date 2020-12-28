8d90c1a11a35dc1ee33c4af8c7b77b709d794b29
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2020-12-28 15:39:37 +0900

    トップページに遷移

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,6 +1,8 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   before_action :group_item, only: [:index, :create]
   def index
+    redirect_to root_path if current_user.id == @item.user_id || @item.buy
     @home_buy = HomeBuy.new
   end
 
