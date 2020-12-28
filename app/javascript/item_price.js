eff3802dfedcdda2c8be385b33fae7f6c6e642cf
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2020-12-28 15:36:36 +0900

    表示販売手数料の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-document.addEventListener('load',() => {
+window.addEventListener('load',() => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
