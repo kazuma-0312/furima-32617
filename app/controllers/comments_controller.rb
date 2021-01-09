c7a8198765c7ea81ceff244c5ca25c179c09f8e6
kazuma-0312 <74850915+kazuma-0312@users.noreply.github.com>
2021-01-09 16:57:19 +0900

    コメントコントローラの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,22 @@
 class CommentsController < ApplicationController
   def new
+    @comments = Comment.all
+    @Comment = Comment.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to item_path(@item)
+    else
+      flash.now[:alert] = 'コメントを入力してください'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(comment).permit(:text).merge(user_id, current_user.id, item_id: params[:item_id])
   end
 end
